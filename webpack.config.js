4fd52ce43424ed08606c1536065a41fb1d6cfdea
Moses Samuel <mosamuel98@gmail.com>
2017-02-25 16:05:55 -0800

    setup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
