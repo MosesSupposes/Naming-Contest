57d0f21bafd64b0ac436ab96ad18bf3f899f1799
Moses Samuel <mosamuel98@gmail.com>
2017-02-25 21:39:45 -0800

    add ejs template

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ import express from 'express';
 
 const server = express();
 
+server.set('view engine', 'ejs');
+server.get('/', (req, res) => {
+    res.render('index', {
+        content: 'Hello Express and <em>EJS</em>!'
+    });
+});
+
 server.listen(config.port, () => {
     console.info('Express listening on port ', config.port);
 });
