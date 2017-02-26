6f003080419a6d90eaa48adf66ea1d30d67690ce
Moses Samuel <mosamuel98@gmail.com>
2017-02-25 21:11:42 -0800

    config server

---
+++ server.js
@@ -0,0 +1,14 @@
+import config from './config';
+import apiRouter from './api';
+import express from 'express';
+
+const server = express();
+
+server.listen(config.port, () => {
+    console.info('Express listening on port ', config.port);
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+
