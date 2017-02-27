e850a441717984202ef9ce382aade78f82cc4161
Moses Samuel <mosamuel98@gmail.com>
2017-02-27 03:18:10 -0800

    Implement server rendering with ReactDOMServer

--- server.js
+++ server.js
@@ -8,20 +8,26 @@ const server = express();
 
 server.use(sassMiddleware({
     src: path.join(__dirname, 'sass'),
-    dest: path.join(__dirname, 'public','stylesheets'),
+    dest: path.join(__dirname, 'public'),
 }));
 
 server.set('view engine', 'ejs');
 
+import serverRender from './serverRender';
+
 server.get('/', (req, res) => {
-    res.render('index', {
-        content: '...'
-    });
+    serverRender()
+        .then(content => {
+            res.render('index', {
+                content
+            });
+        })
+        .catch(console.error);
 });
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
-server.listen(config.port, () => {
-  console.info('Express listening on port', config.port);
+server.listen(config.port, config.host, () => {
+    console.info('Express listening on port', config.port);
 });
