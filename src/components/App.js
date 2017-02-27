2d9f33a797a84f644fa40b7661cf25f22a8b8f32
Moses Samuel <mosamuel98@gmail.com>
2017-02-27 00:17:01 -0800

    refactor App component to update from state

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,25 @@ import ContestPreview from './ContestPreview';
 
 class App extends React.Component { 
     state = { 
-        pageHeader: 'Naming Contests'
+        pageHeader: 'Naming Contests',
+        contests: []
     };
+
+    componentDidMount() {
+        // ajax ...
+        this.setState({
+            contests: data.contests 
+        });
+    }
 
     render() {
         return (
             <div className="App">
                 <Header message= {this.state.pageHeader} />
                 <div>
-                    {this.props.contests.map(contest => 
-                        <ContestPreview  {...contest} 
-                        key={contest.id} />
+                    {this.state.contests.map(contest => 
+                        <ContestPreview  key={contest.id} {...contest} 
+                         />
                     )}
                     
                 </div>
